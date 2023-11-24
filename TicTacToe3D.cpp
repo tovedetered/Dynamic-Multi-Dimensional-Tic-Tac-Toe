c47275d6e2eebc84e0ced97c063ba785a2baa3aa
tovedetered <spenser@Trevors-MacBook-Pro.local>
2023-11-24 12:22:01 -0500

    Initial commit

---
+++ TicTacToe3D.cpp
@@ -0,0 +1,127 @@
+//
+// Created by Spenser Kramer on 11/23/23.
+//
+
+#include "TicTacToe3D.h"
+
+TicTacToe3D::TicTacToe3D(int numRow, int numCol, int numUp, int inNumWin) {
+    totalSize = numRow*numCol*numUp;
+    width = numCol;
+    depth = numUp;
+    height = numRow;
+    numWin = inNumWin;
+    player = 0;
+    mostRecentMove = {};
+    whoWhere = new int[totalSize];
+    for(int i = 0; i < totalSize; i++){
+        whoWhere[i] = 0;
+    }
+}
+
+int TicTacToe3D::runGame() {
+    int result;
+    whoWon = 0;
+    player = 0;
+    bool gameEnd = false;
+    for(int i = 1; !gameEnd; i++){
+        if(i%2 == 1){
+            player = 1;
+            playerTurn(player);
+        }
+        else if(i%2 == 0){
+            player = 2;
+            playerTurn(player);
+        }
+        if(i/2 > numWin-1){
+            result = whoWin();
+            if(result != 0){
+                gameEnd = true;
+                whoWon = player;
+            }
+        }
+        if(i == totalSize){
+            gameEnd = true;
+            return whoWon = 3;
+        }
+    }
+    return whoWon;
+}
+
+int TicTacToe3D::whoWin() { //non-active player cannot win
+    int result = 0;
+    int mRx = mostRecentMove.x;
+    int mRy = mostRecentMove.y;
+    int mRz = mostRecentMove.z;
+    int counter = 0;
+
+    //row
+    for(int x = 0; x< width; x++){
+        if(whoWhere[access(0 + x, mRy, mRz)] == player){
+            counter++;
+        }
+        if(whoWhere[access(0 + x, mRy, mRz)] != player){
+            counter = 0;
+        }
+    }
+    if(counter >= numWin){
+        return 1;
+    }
+    else{
+        counter = 0;
+    }
+    //column
+    for(int y = 0; y< height; y++){
+        if(whoWhere[access(mRx, 0 + y, mRz)] == player){
+            counter++;
+        }
+        if(whoWhere[access(mRx, 0 + y, mRz)] != player){
+            counter = 0;
+        }
+    }
+    if(counter >= numWin){
+        return 1;
+    }
+    else{
+        counter = 0;
+    }
+    //depth thing
+    for(int z = 0; z< depth; z++){
+        if(whoWhere[access(mRx, mRy, 0+z)] == player){
+            counter++;
+        }
+        if(whoWhere[access(mRx, mRy, 0+z)] != player){
+            counter = 0;
+        }
+    }
+    if(counter >= numWin){
+        return 1;
+    }
+    else{
+        counter = 0;
+    }
+    //need Diagonal and anti-diagonal and 3d diagonal
+    return 0;
+}
+
+void TicTacToe3D::playerTurn(int p) {
+    xyz playerinput{};
+    playerinput = getInput(p);
+    updateBoard(playerinput, p);
+    mostRecentMove = playerinput;
+    drawBoard();
+}
+
+void TicTacToe3D::updateBoard(xyz pin, int p) {
+    int x = pin.x;
+    int y = pin.y;
+    int z = pin.z;
+    whoWhere[access(x,y,z)] = p;
+}
+
+int TicTacToe3D::access(int x, int y, int z) const {
+    return (x+ width *(y+depth*z));
+}
+
+TicTacToe3D::~TicTacToe3D() {
+    delete whoWhere;
+}
---
+++ TicTacToe3D.h
@@ -0,0 +1,40 @@
+//
+// Created by Spenser Kramer on 11/23/23.
+//
+
+#ifndef INC_3_DTIC_TAC_TOE_TICTACTOE3D_H
+#define INC_3_DTIC_TAC_TOE_TICTACTOE3D_H
+
+struct xyz{
+    int x;
+    int y;
+    int z;
+};
+
+class TicTacToe3D { //Will just contain logic. ie, nums stored as ints, where other program can display diff
+public:
+    explicit TicTacToe3D(int numRow = 3, int numCol = 3, int numUp = 3, int innumWin = 3);
+    ~TicTacToe3D();
+    int runGame();
+protected:
+    int* whoWhere = nullptr;
+    int width;
+    int depth;
+    int height;
+
+    virtual void drawBoard() = 0;
+    virtual xyz getInput(int p) = 0;
+    [[nodiscard]] int access(int x, int y, int z) const;
+private:
+    int numWin;
+    int totalSize;
+    xyz mostRecentMove{};
+    int whoWin(); //0=none, 1=win, 2=tie
+    int whoWon = 0; //1=1, 2=2, 3=tie
+    int player;
+    void playerTurn(int p);
+    void updateBoard(xyz pin, int p);
+};
+
+
+#endif //INC_3_DTIC_TAC_TOE_TICTACTOE3D_H
